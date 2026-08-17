dc5f0ed003f9d0382222227a6f118154aeac3b58
Ahsan Abbasi <mahsan.abbasi@gmail.com>
2026-08-18 03:42:38 +0500

    Implement user authentication and token management with JWT

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,31 @@ import {User} from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import {ApiResponse} from "../utils/ApiResponse.js";
+import { availableMemory } from "node:process";
+import jwt from 'jsonwebtoken' 
+import { log } from "node:console";
+import { lookup } from "node:dns";
+
+
+const generateAccessAndRefreshTokens = async(userId) => {
+  try{
+    const user = await User.findById(userId);
+    const accessToken = user.generateAccessToken();
+    const refreshToken =  user.generateRefreshToken();
+
+    
+    
+
+    user.refreshToken = refreshToken;
+    await user.save({validateBeforeSave: false}); // validate nhi krne ki zarorat bs save hojao warna hame sari req fileds bhi deni prti hn .save() me
+
+    return {accessToken, refreshToken}
+
+  } catch(error)
+  {
+    throw new ApiError(500, "Something went wrong while generating tokens")
+  }
+}
 
 const registerUser = asyncHandler(async (req, res) => {
 //get user data from request body 
@@ -36,6 +61,8 @@ const registerUser = asyncHandler(async (req, res) => {
   }
     const avatarLocalPath = req.files?.avatar?.[0]?.path;
   const coverImageLocalPath = req.files?.coverImage?.[0]?.path;
+
+  // Ye tab hota hai jab upload.fields([{name: 'avatar'}, {name: 'coverImage'}]) ho — multiple named fields expect karta hai, isliye req.files ek object hai jisme har field naam ke against array hai (req.files.avatar = array of files uploaded under "avatar" key), isliye [0] se pehli file nikalni padti hai.
 
   
 
@@ -70,10 +97,409 @@ const coverImage = coverImageLocalPath ? await uploadOnCloudinary(coverImageLoca
 });
 
 const loginUser = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    success: true,
-    message: "Login controller is working fine",
+  //email pass
+  //check if user available
+  //if not throw error
+  //else generate access and refresh token
+  //send cookies
+
+  const {email, username, password} = req.body;
+ 
+
+  if (!username && !email)
+  {
+    throw new ApiError(400, "Username or Email required")
+  }
+
+  const user = await User.findOne({
+    $or: [{ email }, { username }]
   });
+
+  if (!user)
+  {
+    throw new ApiError(404, "User does not exist");
+  }
+
+  const isPasswordValid = await user.isPasswordCorrect(password);
+
+    if (!isPasswordValid)
+  {
+    throw new ApiError(404, "Invalid user credentials");
+  }
+
+  const {accessToken, refreshToken} = await generateAccessAndRefreshTokens(user._id);
+
+  const loggedInUser = await User.findById(user._id).select("-password -refreshToken"); //1. ye hm doabara userkyn mangwa rhe hn or password or ref token ko minus kyn krte hn -  sab fields lao, SIRF password aur refreshToken chhod kar
+
+  //only server can modify
+ // httpOnly: true — matlab ye cookie JavaScript se access nahi ho sakti (browser ke document.cookie se bhi nahi)
+  const options = {
+    httpOnly: true,
+    secure: true
+  }
+
+  //sending in cookies
+  return res
+  .status(200)
+  .cookie("accessToken", accessToken, options)
+  .cookie("refreshToken", refreshToken, options)
+  .json(
+    new ApiResponse(200,
+    "User Logged in Successfully",
+     {
+      user: loggedInUser, accessToken, refreshToken //2. yaha pr ye kyn bheja ha is ka kia kaam ha  - taake flexible clients (mobile, ya cookie-restricted environments) bhi kaam kar sakein.
+    },  
+  ))
 });
 
-export { registerUser, loginUser };
+const logoutUser = asyncHandler(async (req, res) => {
+  const id = req.user._id;
+  await User.findByIdAndUpdate( //3. 
+    id,{
+      $set: {
+        refreshToken: undefined  //Database mein refreshToken ko khali/undefined kar diya
+      }
+    },
+    {
+      new: true  // ye new true kia ha -  update ke BAAD wala naya document return karo
+    }
+  )
+
+  const options = {
+    httpOnly: true,
+    secure: true
+  }
+  return res
+  .status(200)
+  .clearCookie("accessToken", options)
+  .clearCookie("refreshToken", options)
+  .json(new ApiResponse(201, "User Logged out successfully", {}))
+
+
+})
+
+const refreshAccessToken  = asyncHandler(async (req, res) => {
+  const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken;
+
+  if (!incomingRefreshToken)
+  {
+    throw new ApiError(401, "unauthorised request");
+  }
+  try {
+    const decodedToken = jwt.verify(incomingRefreshToken, process.env.REFRESH_TOKEN_SECRET) // ye kia kr rha ha - Ye decode + validate dono ek sath karta hai: Expiry check, signature check, object de deta ha jis me payload hote hn 
+
+  
+    
+  
+    const user = await User.findById(decodedToken?._id)
+  
+      if (!user)
+      {
+        throw new ApiError(401, "Invalid refresh token")
+      }
+      if (incomingRefreshToken !== user?.refreshToken)
+      {
+        throw new ApiError(401, "Refresh token is expired or used");
+      }
+  
+      const options = {
+        httpOnly: true,
+        secure: true
+      }
+  
+const {accessToken, refreshToken: newRefreshToken} = await generateAccessAndRefreshTokens(user._id);
+
+
+
+  
+      return res
+      .status(201)
+      .cookie("accessToken", accessToken, options)
+      .cookie("refreshToken", newRefreshToken, options)
+      .json(
+        new ApiResponse(200, "Access Token Refreshed",{accessToken, refreshToken : newRefreshToken})
+      )
+  } catch (error) {
+    throw new ApiError(401, error?.message || "Invalid refresh token")
+    
+  }
+
+    
+})
+
+const changeCurrentPassword = asyncHandler(async (req, res) => {
+  const {oldPassword, newPassword} = req.body;
+
+
+  const user = await User.findById(req.user?._id);
+
+  const isPasswordCorrect = await user.isPasswordCorrect(oldPassword);
+
+  if (!isPasswordCorrect)
+  {
+    throw new ApiError(400, "Invalid old Password");    
+  };
+
+  user.password = newPassword;
+
+  await user.save({validateBeforeSave: false});
+
+  res
+  .status(200)
+  .json(
+    new ApiResponse(200, "Password changed Successfully", {})
+  )
+
+
+})
+
+const getCurrentUser = asyncHandler(async (req, res) => {
+  return res
+  .status(200)
+  .json(
+    new ApiResponse(200, "Current user fetched successfully", req.user)
+  )
+})
+
+const updateAccountDetails = asyncHandler(async(req, res) => {
+  const {fullName, email} = req.body;
+  if (!fullName || !email)
+  {
+    throw new ApiError(400, "All fields are required");
+
+  }
+
+  const user = await User.findByIdAndUpdate(
+    req.user?._id,
+    {
+      $set: {
+        fullName,
+        email,
+      }
+    },
+    {new: true}  // is se updated information return hoti ha
+
+  ).select("-password") // user ko data bhjte waqt password nhi bhjte hn
+
+  return res
+  .status(200)
+  .json(
+    new ApiResponse(200, "Account details updated successfully",user)
+  )
+})
+
+const updateUserAvatar = asyncHandler(async (req, res) => {
+  const avatarLocalPath = req.file?.path  // yaha file is lye use kiya kyn k hm ek file upload krwa rhe hn  - Ye tab use hota hai jab route mein upload.single('avatar') middleware ho — sirf ek file expect karta hai, wo seedha req.file (singular) mein milti hai
+
+  if (!avatarLocalPath)
+  {
+    throw new ApiError(400, "Avatar file is missing");
+  }
+
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+
+  
+  if (!avatar) {
+    throw new ApiError(500, "Failed to upload image to Cloudinary");
+  } 
+
+  const user = await User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $set: {
+        avatar: avatar.url
+      }
+    },
+    {new: true}
+
+  ).select("-password")
+
+    return res
+  .status(200)
+  .json(
+    new ApiResponse(200, "Avatar updated successfully",user)
+  )
+
+})
+
+const updateUserCoverImage = asyncHandler(async (req, res) => {
+  const coverImageLocalPath = req.file?.path  // yaha file is lye use kiya kyn k hm ek file upload krwa rhe hn 
+
+  if (!coverImageLocalPath)
+  {
+    throw new ApiError(400, "Cover Image file is missing");
+  }
+
+  //how can i delete old image
+
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+  
+  if (!coverImage) {
+    throw new ApiError(500, "Failed to upload image to Cloudinary");
+  } 
+
+  const user = User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $set: {
+        coverImage: coverImage.url
+      }
+    },
+    {new: true}
+
+  ).select("-password")
+
+    return res
+  .status(200)
+  .json(
+    new ApiResponse(200, "Cover Image updated successfully",user)
+  )
+
+})
+const getUserChannelProfile = asyncHandler(async(req, res) => {
+  const {username} = req.params;
+  if(!username.trim()){
+    throw new ApiError(400, "username is missing")
+  }
+
+ const channel = await User.aggregate([
+{
+  $match: {
+    username: username?.toLowerCase()
+  }
+},
+{
+  lookup:{
+    from:"subscriptions",
+    localField: "_id",
+    foreignField: "channel",
+    as: "subscribers"
+
+  }
+},
+{
+  lookup:{
+    from:"subscriptions",
+    localField: "_id",
+    foreignField: "subscriber",
+    as: "subscribedTo"
+
+  }
+},
+{
+  $addFields: {
+    subscribersCount: {
+      $size: "$subscribers"
+    },
+    channelSubscribedToCount: {
+      $size: "$subscribedTo"
+    },
+    isSubscribed: {
+      $cond: {
+        if: {$in: [req.user?._id, "$subscribers.subscriber"]},
+        then: true,
+        else: false
+      }
+    }
+  }
+},
+{
+  $project : {
+    fullName: 1,
+    username: 1,
+    subscribersCount: 1,
+    channelSubscribedToCount: 1,
+    isSubscribed: 1,
+    avatar: 1,
+    coverImage: 1,
+    email: 1
+  }
+}
+])
+
+console.log(channel) // is ka jawab kia aaega, ye aggregate return kia krta ha 
+
+if(!channel?.length){
+  throw new ApiError(404, "Chanel does not exist")
+}
+
+return res
+.status(200)
+.json(
+  new ApiResponse(200,"User channel fetched successfully" , channel[0])
+)
+
+})
+
+const getWatchHistory = asyncHandler(async(req, res) => {
+  const user = await User.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(req.user._id)
+        // req.user._id direct use nhi kr skte yaha mongose kaam nhi kre ga is lye mongose ki object id banai pare gi , 
+        // ye new kya hota ha or ye yaha kia kr rha ha 
+      }
+
+    },
+    {
+      $lookup:{
+        from: "videos",
+        localField: "watchHistory",
+        foreignField: "_id",
+        as: "watchHistory",
+        pipeline: [  //sub aggregate
+          {
+            $lookup:
+            {
+              from: "users",
+              localField: "owner",
+              foreignField: "_id",
+              as: "owner",
+              pipeline: [
+                {
+                  $project: {
+                    fullName: 1,
+                    email: 1,
+                    avatar: 1,
+
+                  }  // is ko me agr dosre lookup k bahar likhta to kia hota?
+                }
+              ]
+            }
+          },
+          {
+            $addFields: {
+              owner: {
+                $first: "$owner"
+              }
+
+              //frontend wala ab owner.fullName, etc access krskta ha
+            }
+          }
+        ]
+      }
+    },
+
+
+  ])
+  return re
+  .status(200)
+  .json(
+    new ApiResponse(200, "Watch History fetched successfully",user[0].watchHistory)  
+    //ye yaha user[0] bh to bhjskte the ye .watch history kyn lagaya 
+  )
+})
+
+
+
+
+
+
+
+export { registerUser, loginUser, logoutUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage, getUserChannelProfile, getWatchHistory};
+
+
+
+  
+
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,13 @@
 import {Router} from "express";
-import { registerUser, loginUser } from "../controllers/user.controller.js";
-import { upload } from "../middlewares/multer.middleware.js";  
+import { registerUser,
+   loginUser,
+    logoutUser,
+     refreshAccessToken,
+      changeCurrentPassword, getCurrentUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage, getUserChannelProfile, getWatchHistory} from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js"; 
+import { verifyJWT } from "../middlewares/auth.middleware.js"; 
+
+
 const router = Router();
 
  router.route("/register").post(
@@ -9,7 +16,28 @@ const router = Router();
    { name: "coverImage", maxCount: 1 }
   ]),
    registerUser);
+
   router.route("/login").post(loginUser);
+
+  //secured routes - user login hona chahye
+  router.route("/logout").post(verifyJWT, logoutUser) // pehle jwt chale ga phir logoutUser
+  router.route("/refresh-token").post(refreshAccessToken)
+
+  router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+  router.route("/current-user").get(verifyJWT, getCurrentUser);
+  router.route("/update-account").patch(verifyJWT, updateAccountDetails);
+ // patch kb use krte hn
+  router.route("/avatar").patch(verifyJWT,upload.single("avatar"),  updateUserAvatar);
+  //patch shayad is lye use krte jb hame kch cheezein update krani ho
+
+  router.route("/coverImage").patch(verifyJWT,upload.single("coverImage"),  updateUserCoverImage);
+
+ router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
+  router.route("/history").get(verifyJWT, getWatchHistory);
+
+  
+
+  
 
 
 export default router;
